9872ff90cb99fc8444a2cdcd603ec85aa8862358
Bogdan Onatsky <15bogdan17@gmail.com>
2019-04-22 17:58:42 +0300

    Complete editing task

--- client/src/components/Dashboard/components/Task/Task.jsx
+++ client/src/components/Dashboard/components/Task/Task.jsx
@@ -40,7 +40,12 @@ class Task extends Component {
       isAlive: true,
       name: this.props.data.name,
       id: this.props.data.id,
+      status: this.props.data.status,
+      deadline: this.props.data.deadline,
+      validation: this.validator.valid(),
     };
+
+    console.log(this.state);
 
     this.submitted = false;
     this.clickDelete = this.clickDelete.bind(this);
@@ -66,6 +71,7 @@ class Task extends Component {
     event.preventDefault();
 
     let {name, deadline} = this.state;
+    console.log(this.state);
 
     const validation = this.validator.validate(this.state);
     this.setState({validation});
@@ -75,41 +81,68 @@ class Task extends Component {
       editTask({
         name,
         deadline,
-        project_id: `${this.state.project_id}`,
+        id: `${this.state.id}`,
+        status: `${this.state.status ? 0: 1}`,
       })
         .then((res) => {
-          const tasks = this.state.tasks || [];
           let date = new Date(res.info.deadline).toLocaleString('en-US', {
             year: 'numeric',
             month: 'numeric',
             day: 'numeric',
           });
-          tasks.push({
-            deadline: date,
-            id: res.info.id,
-            name: res.info.name,
-            status: res.info.status,
-          });
-          console.log(tasks);
 
           this.setState({
-            tasks: tasks,
+            visibleEditModal: !this.state.visibleEditModal,
           });
         })
         .catch(error => console.log(error));
     }
   };
 
+  handlerEdit = () => {
+    this.setState({
+      visibleEditModal: !this.state.visibleEditModal,
+    })
+  };
+
+  handlerCloseModal = (event) => {
+    if (event.target.classList.contains('modal')) {
+      this.setState({
+        visibleEditModal: !this.state.visibleEditModal,
+        name: this.props.data.name,
+      })
+    }
+  };
+
   render() {
-    const {name, deadline} = this.state.data;
+    const {name, deadline} = this.state;
+    let validation = this.submitted ?
+      this.validator.validate(this.state) :
+      this.state.validation;
 
     return (
       this.state.isAlive ? (
       <li className="task">
         <h4 className="task__title">{name}</h4>
         <div className="task__date">{deadline}</div>
-        <button className="btn btn-color">Edit task</button>
+        <button className="btn btn-color" onClick={this.handlerEdit}>Edit task</button>
         <button className="btn btn-danger" onClick={this.clickDelete}>Delete task</button>
+        {this.state.visibleEditModal === true ? (
+          <div className="modal" onClick={this.handlerCloseModal}>
+            <form className="modal__container" noValidate onSubmit={this.handlerSubmit}>
+              <p className="title modal__title">Edit <span className="modal__color">{name}</span></p>
+              <div className={validation.name.isInvalid ? 'input error modal__input' : 'input modal__input'} data-error={validation.name.message}>
+                <input type="text" name="name" onInput={this.handlerInput} required placeholder="Enter new name" className="input__field"/>
+                <label className="input__label">Enter new name</label>
+              </div>
+              <div className={validation.deadline.isInvalid ? 'input error modal__input' : 'input modal__input'} data-error={validation.deadline.message}>
+                <input type="date" name="deadline" onInput={this.handlerInput} required placeholder="Enter new name" className="input__field"/>
+                <label className="input__label">Deadline</label>
+              </div>
+              <button className="btn btn-color">Save</button>
+            </form>
+          </div>
+        ) : (null)}
       </li>
       ) : ''
     );
