9a5ba6b26d895ac57b612ab8251bcfff14bee1d8
Bohdan Onatsky <15bogdan17@gmail.com>
2019-04-22 04:23:41 +0300

    Pre build for heroku

---
+++ client/build/precache-manifest.ec3477bff8c94fdbedd20cfa5dbb29d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ca315ece63057c0c314",
+    "url": "/static/js/main.778aca93.chunk.js"
+  },
+  {
+    "revision": "543d5b35eb404dbb2c58",
+    "url": "/static/js/2.efd54737.chunk.js"
+  },
+  {
+    "revision": "4ca315ece63057c0c314",
+    "url": "/static/css/main.cbc39b92.chunk.css"
+  },
+  {
+    "revision": "62e64e9e33e376df8c02325f1e58277d",
+    "url": "/index.html"
+  }
+];
