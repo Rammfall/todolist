660fc2e4a16cbf9a4fe5631d16f230f9f1e11e4d
Bohdan Onatsky <15bogdan17@gmail.com>
2019-05-04 18:21:51 +0300

    add gzip compression to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 const app = express();
 const router = express.Router();
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const Users = require('./routes/Users');
